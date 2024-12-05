09d500a6856902909c258acb2abcfffef7cebc23
joaocandidozup <joao.candido@zup.com>
2024-12-05 13:50:23 -0300

    feat: creates the support variables

---
+++ src/ContarDigitos.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ContarDigitos {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("digite um numero inteiro ");
+        int numero = entrada.nextInt();
+        int contadorDeDigitos = 0;
+
+
+    }
+}
