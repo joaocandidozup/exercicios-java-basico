6ba92e69837b05f841869859e2d0fee1939e9c63
joaocandidozup <joao.candido@zup.com>
2024-12-05 10:38:51 -0300

    feat:add exercise on repetition structure (do while)

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        int[] numeros = {10,20,30,40,50};
+        int[] numeros = {101,201,301,401,501};
         /*for
         for(int i=0;i<numeros.length;i++){
             System.out.println(numeros[i]);
         }
 
-        while*/
+        while
         int i = 0;
         while (i<5){
             System.out.println(numeros[i]);
             i++;
         }
+
+        do while*/
+        int index = 0;
+        do {
+            System.out.println(numeros[index]);
+            index++;
+        } while(index<5);
+
+
+
     }
 }
