79a4a2b77a081947fc9d4eb63ddd6bb80be7b087
joaocandidozup <joao.candido@zup.com>
2024-12-05 11:58:21 -0300

    feat:add exercise adding numbers

---
+++ src/SomaDeNumeros.java
@@ -0,0 +1,10 @@
+public class SomaDeNumeros {
+    public static void main(String[] args) {
+        int n = 5;
+        int soma = 0;
+        for(int i = 1;i<=n;i++){
+            soma += i;
+        }
+        System.out.println(soma);
+    }
+}
