e946cc9b0c1ce9e12d680b9f16816b00a7b15dfe
joaocandidozup <joao.candido@zup.com>
2024-12-05 10:06:46 -0300

    feat:add exercises on repetition structure for

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] numeros = {1,2,3,4,5};
+        for(int i=0;i<numeros.length;i++){
+            System.out.println(numeros[i]);
+        }
     }
 }
