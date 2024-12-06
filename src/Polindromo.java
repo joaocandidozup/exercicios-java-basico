f50a77e02e8d4e6b19cd426c6ce8eeddf8756b8b
joaocandidozup <joao.candido@zup.com>
2024-12-06 11:09:13 -0300

    feat: add polydrome exercise

--- src/Polindromo.java
+++ src/Polindromo.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Polindromo {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        System.out.print("digite um numero inteiro: ");
+        int numero = leitura.nextInt();
+        int aux = numero;
+        int numeroInvertido = 0;
+        boolean ehPolindromo = true;
+
+        while (numero != 0){
+        int digito = numero % 10;
+        numeroInvertido = (numeroInvertido * 10) + digito;
+        numero /= 10;
+        }
+        if(aux == numeroInvertido){
+            ehPolindromo = true;
+        }else{
+            ehPolindromo = false;
+        }
+
+        System.out.println(ehPolindromo);
+    }
 }
