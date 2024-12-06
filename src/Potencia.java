e4bbde3203a4d99d48cb445afc96f802621a2bce
joaocandidozup <joao.candido@zup.com>
2024-12-06 11:06:42 -0300

    feat: add exercise strength

--- src/Potencia.java
+++ src/Potencia.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Potencia {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        System.out.print("digite o numero base: ");
+        int base = leitura.nextInt();
+        System.out.print("digite o expoente: ");
+        int expoente = leitura.nextInt();
+        int resposta = 1;
+
+        for(int i =1;i<= expoente;i++){
+           resposta *= base;
+        }
+        System.out.println(resposta);
+
+    }
 }
